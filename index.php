e6e58ef5543da41206e42ed8f9fb0ecede0406f4
sirtaj786 <ksirtaj29@gmail.com>
2023-04-28 15:40:41 +0530

    crud operation1

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+   
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
+    
    
 
     <title>Hello, world!</title>
